121729f1e212a95427ad5d4581828b2fd944e029
RiceNLamb <lammaurice12345@gmail.com>
2021-05-15 21:31:12 +1000

    changed layout of buttons in gui

--- gui.py
+++ gui.py
@@ -1,25 +1,50 @@
 import PyQt5.QtWidgets as wid
-
+from PyQt5.QtGui import QPixmap 
+import sys
 class MainWindow(wid.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Finding friends")
         self.setLayout(wid.QVBoxLayout())
         self.resize(1000,500)
-        self.dialoguelayout()
+        self.playing_layout()
         self.show()
         
-    def dialoguelayout(self):
-        self.container = wid.QWidget()
-        self.container.setLayout(wid.QGridLayout())
-        
+    def playing_layout(self):
         dialoguebutton_1 = wid.QPushButton("sample text1")
         dialoguebutton_2 = wid.QPushButton("sample text2")
         dialoguebutton_3 = wid.QPushButton("sample text3")
         
-        self.container.layout().addWidget(dialoguebutton_1,3,2,1,6)
-        self.container.layout().addWidget(dialoguebutton_2,4,2,1,6)
-        self.container.layout().addWidget(dialoguebutton_3,5,2,1,6)
+
+
+        self.container = wid.QWidget()
+        self.container.setLayout(wid.QGridLayout())
+
+        pixmap2 = QPixmap('tester.jpg')
+        self.image2 = wid.QLabel(self.container)
+        self.image2.setPixmap(pixmap2)
+
+
+
+            
+        self.top_row = wid.QWidget()
+        self.top_row.setLayout(wid.QHBoxLayout())
+        self.top_row.layout().addWidget(dialoguebutton_1)
+        
+        self.container.layout().addWidget(self.top_row)
+
+            
+        self.middle_row = wid.QWidget()
+        self.middle_row.setLayout(wid.QHBoxLayout())
+        self.middle_row.layout().addWidget(dialoguebutton_2)
+        
+        self.container.layout().addWidget(self.middle_row)
+        
+        self.bottom_row = wid.QWidget()
+        self.bottom_row.setLayout(wid.QHBoxLayout())
+        self.bottom_row.layout().addWidget(dialoguebutton_3)
+        
+        self.container.layout().addWidget(self.bottom_row)
         
         self.layout().addWidget(self.container)
 
