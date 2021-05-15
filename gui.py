a54e015320503627e4a3fd7a4878faff57a9f07f
RiceNLamb <lammaurice12345@gmail.com>
2021-05-15 22:15:36 +1000

    Functioning gameplay gui

--- gui.py
+++ gui.py
@@ -1,11 +1,11 @@
 import PyQt5.QtWidgets as wid
-from PyQt5.QtGui import QPixmap 
+from PyQt5.QtGui import *
+from PyQt5.QtCore import * 
 import sys
 class MainWindow(wid.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Finding friends")
-        self.setLayout(wid.QVBoxLayout())
         self.resize(1000,500)
         self.playing_layout()
         self.show()
@@ -15,38 +15,41 @@ class MainWindow(wid.QWidget):
         dialoguebutton_2 = wid.QPushButton("sample text2")
         dialoguebutton_3 = wid.QPushButton("sample text3")
         
+        self.container = wid.QGridLayout()
+        
+        self.picture_raw = QPixmap('tester.jpg')
+        self.picture_resized = self.picture_raw.scaled(500, 450, Qt.KeepAspectRatio)
+        self.picture_final = wid.QLabel()
+        self.picture_final.setPixmap(self.picture_resized)
 
+        self.container.layout().addWidget(self.picture_final)
 
-        self.container = wid.QWidget()
-        self.container.setLayout(wid.QGridLayout())
+        self.character_dialogue = wid.QLabel("please work")
+        self.container.layout().addWidget(self.character_dialogue) 
 
-        pixmap2 = QPixmap('tester.jpg')
-        self.image2 = wid.QLabel(self.container)
-        self.image2.setPixmap(pixmap2)
-
-
-
-            
+      
         self.top_row = wid.QWidget()
-        self.top_row.setLayout(wid.QHBoxLayout())
-        self.top_row.layout().addWidget(dialoguebutton_1)
+        self.top_row.setLayout(wid.QGridLayout())
+        self.top_row.layout().addWidget(dialoguebutton_1,1,1,1,1)
         
         self.container.layout().addWidget(self.top_row)
 
             
         self.middle_row = wid.QWidget()
-        self.middle_row.setLayout(wid.QHBoxLayout())
-        self.middle_row.layout().addWidget(dialoguebutton_2)
+        self.middle_row.setLayout(wid.QGridLayout())
+        self.middle_row.layout().addWidget(dialoguebutton_2,1,1,1,1)
         
         self.container.layout().addWidget(self.middle_row)
         
         self.bottom_row = wid.QWidget()
-        self.bottom_row.setLayout(wid.QHBoxLayout())
-        self.bottom_row.layout().addWidget(dialoguebutton_3)
+        self.bottom_row.setLayout(wid.QGridLayout())
+        self.bottom_row.layout().addWidget(dialoguebutton_3,1,1,1,1)
         
         self.container.layout().addWidget(self.bottom_row)
+
+        self.setLayout(self.container)
         
-        self.layout().addWidget(self.container)
+
 
 
 
