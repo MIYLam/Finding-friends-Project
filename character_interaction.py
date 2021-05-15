225f9e77b5b6bb9fff714adfae55037c6e0ceb9d
RiceNLamb <lammaurice12345@gmail.com>
2021-05-15 10:56:41 +1000

    Optimised character object

--- character_interaction.py
+++ character_interaction.py
@@ -1,22 +1,21 @@
 import csv
 
-morning_questions = open("morningquestions.csv", "r")
+morning_file = open("morningquestions.csv", "r")
 
-class Questions:
-    def __init__(self, question_csv):
-        self.question_csv = question_csv
+class Questions:            #Using to 
+    def __init__(self, questions):
+        self.questions = questions
 
     def questions_reader(self):
         question_list = []
-        reader = csv.reader(self.question_csv, delimiter=',')
+        reader = csv.reader(self.questions, delimiter=',')
         line_counter = 0
         for row in reader:
             if line_counter == 0:
                 line_counter = line_counter + 1
-                pass
             else:
                 question_list.append(row[1])
 
         return question_list
 
-morning = Questions(morning_questions)
+morning_questions = Questions(morning_file) #These variables need to be interated throuhg a for loop 
